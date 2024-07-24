2aa7c92c2ab73258a7672fb14a059b57f495e6a4
Akhilesh1712 <148992157+Akhilesh1712@users.noreply.github.com>
2024-07-24 23:31:32 +0530

    new7

--- Frontend/src/components/common/Posts.jsx
+++ Frontend/src/components/common/Posts.jsx
@@ -1,23 +1,62 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import {useQuery} from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({feedPost}) => {
+	
+
+ 
+	const getPostEndpoints = () =>{
+		switch (feedPost) {
+			case "forYou":
+				return "/api/posts/all"
+				break;
+		    case "following":
+				return "/api/posts/following";
+				break;
+			default:
+				return "/api/posts/all";
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndpoints();
+
+	const {data,isLoading , refetch , isRefetching} = useQuery({
+            queryKey: ["posts"],
+			queryFn: async() => {
+				try {
+					const res = await fetch(POST_ENDPOINT);
+					const data = await res.json();
+
+					if(!res.ok){
+						throw new Error(data.error || "Something went wrong in it");
+					}
+
+					return data;
+				} catch (error) {
+					throw new Error(error);
+				}
+			},
+	})
+
+	useEffect(() =>{
+		refetch();
+	},[feedPost , refetch]); //when ever feestype chages means click to foryou or following is shoud refecth or re re-render it because agar foryuo per pahale to vo switch to for you ke leye chal gya ab ager following per jaye ge to dobara chalena padega
 
 	return (
 		<>
-			{isLoading && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && data?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && data && (
 				<div>
-					{POSTS.map((post) => (
+					{data.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
 				</div>
--- Frontend/src/pages/home/HomePage.jsx
+++ Frontend/src/pages/home/HomePage.jsx
@@ -37,7 +37,7 @@ const HomePage = () => {
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts feedPost={feedType} />
 			</div>
 		</>
 	);
