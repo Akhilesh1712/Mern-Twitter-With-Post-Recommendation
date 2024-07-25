80f1225f7da6418be0c602932bfb04ff73dab194
Akhilesh1712 <148992157+Akhilesh1712@users.noreply.github.com>
2024-07-26 00:43:43 +0530

    new13

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,20 +39,20 @@ function App() {
         )
       }
 
-  return (
-    <div className='flex max-w-6xl mx-auto'>
-      {authUser && < Sidebar/>}
-			<Routes>
-				<Route path='/' element={ authUser ?  <HomePage /> : <Navigate to="/login" />} />
-				<Route path='/signup' element={ !authUser ? <SignUpPage /> :<Navigate to="/" /> } />
-				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path='/notifications' element={!authUser ? <NotificationPage /> : <Navigate to="/login" />} />
-        <Route path='/profile/:username' element={!authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-			</Routes>
-      {authUser && <RightPanel/>}
-      <Toaster/>
-		</div>
-  )
+      return (
+        <div className='flex max-w-6xl mx-auto'>
+          {authUser && <Sidebar />}
+          <Routes>
+            <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+            <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+            <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+            <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
+            <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+          </Routes>
+          {authUser && <RightPanel />}
+          <Toaster />
+        </div>
+      );
 }
 
 export default App
--- Frontend/src/pages/notification/NotificationPage.jsx
+++ Frontend/src/pages/notification/NotificationPage.jsx
@@ -1,52 +1,70 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
 import { MdDeleteOutline } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
-const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+const NotificationPage = () => {	 
+	const { data: notifications, isLoading } = useQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/notifications");
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		}
+	});
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+	const queryClient = useQueryClient();
+	const { mutate: deleteNotifications, isPending } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch("/api/notifications", {
+					method: "DELETE",
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Deleted");
+			queryClient.invalidateQueries(["notifications"]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		}
+	});
 
 	return (
 		<>
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
 				<div className='flex justify-between items-center p-4 border-b border-gray-700'>
 					<p className='font-bold'>Notifications</p>
-					<div className='dropdown '>
-						<div tabIndex={0} role='button' className='m-1'>
-							<MdDeleteOutline className='w-4 size-10' />
+					<div className='dropdown'>
+						<div
+							tabIndex={0}
+							role='button'
+							className='m-1 cursor-pointer'
+						>
+							<MdDeleteOutline className='w-4 h-4' />
 						</div>
 						<ul
 							tabIndex={0}
 							className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'
 						>
 							<li>
-								<a onClick={deleteNotifications}>Delete all notifications</a>
+								<a onClick={() => deleteNotifications()}>Delete all notifications</a>
 							</li>
 						</ul>
 					</div>
@@ -65,7 +83,7 @@ const NotificationPage = () => {
 							<Link to={`/profile/${notification.from.username}`}>
 								<div className='avatar'>
 									<div className='w-8 rounded-full'>
-										<img src={notification.from.profileImg || "/avatar-placeholder.png"} />
+										<img src={notification.from.profileImg || "/avtar_place_holder.png"} />
 									</div>
 								</div>
 								<div className='flex gap-1'>
@@ -80,4 +98,5 @@ const NotificationPage = () => {
 		</>
 	);
 };
+
 export default NotificationPage;
