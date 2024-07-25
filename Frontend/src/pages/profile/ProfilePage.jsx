f691ca659b661a562d1767fc54b6eb50a469554f
Akhilesh1712 <148992157+Akhilesh1712@users.noreply.github.com>
2024-07-26 01:58:51 +0530

    new15

--- Frontend/src/pages/profile/ProfilePage.jsx
+++ Frontend/src/pages/profile/ProfilePage.jsx
@@ -11,8 +11,11 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { formatMemberSinceDate } from "../../utils/date/function";
+import useFollow from "../../components/hooks/useFollow";
+import toast from "react-hot-toast";
+
 
 const ProfilePage = () => {
 
@@ -25,10 +28,11 @@ const ProfilePage = () => {
 	const profileImgRef = useRef(null);
 
     const {username} = useParams();
-   
-    
 
-	const isMyProfile = true;
+	const queryClient = useQueryClient();
+
+	const {follow,isPending} =  useFollow();
+    const {data: authUser} = useQuery({queryKey: ["authUser"]})
     
 	const {data:user,isLoading , refetch , isRefetching} = useQuery({
         queryKey: ["userProfile"],
@@ -58,7 +62,45 @@ const ProfilePage = () => {
 			reader.readAsDataURL(file);
 		}
 	};
+   
 
+	const {mutate:updatProfile,isPending: isUpdating} = useMutation({
+		mutationFn: async() => {
+			try {
+				const res = await fetch(`/api/users/update`,{
+					method: "POST",
+					headers: {
+						"Content-Type" : "application/json",
+					},
+					body: JSON.stringify({
+						coverImg,
+						profileImg
+					}),
+
+				})
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something Wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Updated");
+			Promise.all([
+				queryClient.invalidateQueries({queryKey: ["authUser"]}),
+				queryClient.invalidateQueries({queryKey:["userProfile"]}),
+			])
+		},
+		onError: ()=>{
+			toast.error(error.message);
+		}
+	}) 
+	const isMyProfile = authUser._id === user?._id;
+
+	const amIfollowing = authUser?.following.includes(user?._id);
 	useEffect(() =>{
 		refetch()
 	},[username,refetch,username]);
@@ -126,21 +168,24 @@ const ProfilePage = () => {
 								</div>
 							</div>
 							<div className='flex justify-end px-4 mt-5'>
-								{isMyProfile && <EditProfileModal />}
+								{isMyProfile && <EditProfileModal authUser={authUser} />}
 								{!isMyProfile && (
 									<button
 										className='btn btn-outline rounded-full btn-sm'
-										onClick={() => alert("Followed successfully")}
+										onClick={() => follow(user?._id)}
 									>
-										Follow
+										{isPending && "LOADING...."}
+										{!isPending && amIfollowing && "Unfollow"}
+										{!isPending && !amIfollowing && "Follow"} 
 									</button>
 								)}
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => alert("Profile updated successfully")}
+										onClick={() => updatProfile()}
 									>
-										Update
+										{isUpdating ? "Updating...." : "Update"}
+
 									</button>
 								)}
 							</div>
@@ -153,6 +198,22 @@ const ProfilePage = () => {
 								</div>
 
 								<div className='flex gap-2 flex-wrap'>
+								{user?.link && (
+										<div className='flex gap-1 items-center '>
+											<>
+												<FaLink className='w-3 h-3 text-slate-500' />
+												<a
+													href={user?.link}
+													target='_blank'
+													rel='noreferrer'
+													className='text-sm text-blue-500 hover:underline'
+												>
+		
+													{user?.link}
+												</a>
+											</>
+										</div>
+									)}
 									<div className='flex gap-2 items-center'>
 										<IoCalendarOutline className='w-4 h-4 text-slate-500' />
 										<span className='text-sm text-slate-500'>{membersince}</span>
